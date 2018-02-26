dfa57029bc78b02b5eaf92bf4f84689eabfb4537
Andrei F <soheevich@gmail.com>
2018-02-26 14:38:53 +0300

    Change gulp package

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,26 @@
 const gulp = require('gulp');
 const autoprefixer = require('gulp-autoprefixer');
-const postcss = require('gulp-postcss');
 const uglify = require('gulp-uglify');
-
-gulp.task('default', () =>
-  gulp
-    .src('.src/app.css')
-    .pipe(autoprefixer({
-      browsers: ['last 2 versions'],
-      cascade: false,
-    }))
-    .pipe(gulp.dest('./build/styles')));
-
-gulp.task('css', () => gulp
-  .src('./src/*.css')
-  .pipe(postcss())
-  .pipe(gulp.dest('./build/styles')));
+const sass = require('gulp-sass');
 
 gulp.task('compress', () =>
   gulp
     .src('./src/scripts/*.js')
     .pipe(uglify())
     .pipe(gulp.dest('./build/scripts')));
+
+// Compile Sass
+gulp.task('sass', () =>
+  gulp
+    .src(['src/scss/*.scss'])
+    .pipe(sass({ outputStyle: 'expanded' }).on('error', sass.logError))
+    .pipe(autoprefixer({ browsers: ['last 2 versions'], cascade: false }))
+    .pipe(gulp.dest('src/css')));
+
+// Watch
+gulp.task('watch-css', ['sass'], () => {
+  gulp.watch(['src/scss/*.scss'], ['sass']);
+});
+
+// Default
+gulp.task('default', ['watch-css']);
