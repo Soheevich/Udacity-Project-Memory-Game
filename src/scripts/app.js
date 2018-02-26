c927a46ebf0a7d0e0fc6cdfbd0e36636a367a0a5
Andrei F <soheevich@gmail.com>
2018-02-26 21:46:25 +0300

    Make function to randomize icons

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -4,6 +4,7 @@
 // VARIABLES
 // *****************
 const cards = document.querySelectorAll('.main__card');
+const cardsIcons = document.querySelectorAll('.card__icon');
 const buttonNewGame = document.querySelector('.main__new-game');
 
 
@@ -16,32 +17,29 @@ const clicked = (e) => { e.target.style.background = 'red'; };
 
 // New game
 const newGame = () => {
-  const cardsIndexes = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+  const indexes = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+
+  const shuffleArray = (array) => {
+    const output = array.slice(0);
+    for (let i = array.length - 1; i > 0; i -= 1) {
+      const j = Math.floor(Math.random() * (i + 1));
+      const temp = output[i];
+      output[i] = output[j];
+      output[j] = temp;
+    }
+    return output;
+  };
+
+  const randomCardsIndexes = shuffleArray(indexes);
   const classNames = ['maki-airport', 'maki-art-gallery', 'maki-bar', 'maki-baseball', 'maki-cinema', 'maki-college', 'maki-harbor', 'maki-town-hall'];
-  const newPairs = [];
-  const getRandom = max => Math.floor(Math.random() * max);
 
-  cards.forEach((card) => { card.firstChild.className = 'card__icon'; });
-
-  for (let i = 0; i < 8; i += 1) {
-    let randomIndex = getRandom(cardsIndexes.length);
-
-    newPairs.push(cardsIndexes[randomIndex]);
-    cardsIndexes.splice(randomIndex, 1);
-    randomIndex = Math.floor(Math.random() * (cardsIndexes.length));
-    newPairs.push(cardsIndexes[randomIndex]);
-    cardsIndexes.splice(randomIndex, 1);
-  }
-
-  newPairs.forEach((num, j) => {
-    if (j % 2 === 0) {
-      const className = classNames[j / 2];
-      cards[num].firstChild.classList.add(className);
-      cards[num + 1].firstChild.classList.add(className);
+  randomCardsIndexes.forEach((randomIndex, i) => {
+    if (i % 2 === 0) {
+      cardsIcons[randomIndex].className = `card__icon ${classNames[i / 2]}`;
+    } else {
+      cardsIcons[randomIndex].className = `card__icon ${classNames[(i - 1) / 2]}`;
     }
   });
-
-  console.log(newPairs);
 };
 
 
