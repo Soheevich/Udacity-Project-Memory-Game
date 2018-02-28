a52c06e04239c311450eb64887c5405f8f2f40aa
Andrei F <soheevich@gmail.com>
2018-02-28 09:04:18 +0300

    Add disabling click timeout while playing animation

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -40,74 +40,127 @@ const printMoves = (num) => {
   numberOfTurns.textContent = num + (num === 1 ? ' Move' : ' Moves');
 };
 
-// Local scope for each new game
-const newGame = () => {
+// IIFE for a local scope for a new game
+(function autorun() {
   let activeCard = null;
   let movesNumber = 0;
+  let isRunning = true; // to end the function if player starts a new game
+  let canClick = true; // to prevent clicking on another cards while animation
 
-  // On card click
-  const clicked = (e) => {
-    if (activeCard) {
-      // Every click after selecting any card will be counted as one move
+  // New game
+  const newGame = () => {
+    activeCard = null;
+    movesNumber = 0;
+    isRunning = true;
 
-      // Clicking on the same card will remove its selection
-      if (e.target === activeCard) {
-        activeCard.classList.remove('card__flipped');
-        activeCard = null;
-        movesNumber += 1;
-        printMoves(movesNumber);
+    // On card click
+    const clicked = (e) => {
+      if (activeCard && canClick) {
+        // Every click after selecting any card will be counted as one move
 
-        // Got a pair, it'll mark them and remove event listeners
-      } else if (e.target.querySelector('.card__icon').className === activeCard.querySelector('.card__icon').className) {
-        e.target.classList.add('card__flipped');
-        setTimeout(() => {
-          e.target.classList.add('shake');
-          activeCard.classList.add('shake');
-          activeCard = null;
-        }, 700);
-        e.target.removeEventListener('click', clicked);
-        activeCard.removeEventListener('click', clicked);
-        movesNumber += 1;
-        printMoves(movesNumber);
-
-        // Wrong pair, remove selection from the both cards
-      } else {
-        e.target.classList.add('card__flipped');
-
-        setTimeout(() => {
+        // Clicking on the same card will remove its selection
+        if (e.target === activeCard) {
           activeCard.classList.remove('card__flipped');
-          e.target.classList.remove('card__flipped');
+          canClick = false;
           activeCard = null;
-        }, 700);
+          movesNumber += 1;
+          printMoves(movesNumber);
+          setTimeout(() => {
+            canClick = true;
+          }, 300);
 
-        movesNumber += 1;
-        printMoves(movesNumber);
+          // Got a pair, it'll mark them and remove event listeners
+        } else if (
+          e.target.querySelector('.card__icon').className ===
+          activeCard.querySelector('.card__icon').className
+        ) {
+          e.target.classList.add('card__flipped');
+          canClick = false;
+          setTimeout(() => {
+            e.target.classList.add('shake');
+            activeCard.classList.add('shake');
+            activeCard = null;
+            setTimeout(() => {
+              canClick = true;
+            }, 300);
+          }, 700);
+          e.target.removeEventListener('click', clicked);
+          activeCard.removeEventListener('click', clicked);
+          movesNumber += 1;
+          printMoves(movesNumber);
+
+          // Wrong pair, remove selection from the both cards
+        } else {
+          e.target.classList.add('card__flipped');
+          canClick = false;
+
+          /* Somehow 'shake' and 'card__flipped' transforms are interacting.
+          So I made an extra setTimeout with 100ms to prevent bugs.
+          Other timeouts are needed to change classes, each transformation lasts 700ms */
+          setTimeout(() => {
+            e.target.classList.add('shake');
+            activeCard.classList.add('shake');
+
+            // Do after shake animation
+            setTimeout(() => {
+              activeCard.classList.remove('shake');
+              e.target.classList.remove('shake');
+              setTimeout(() => {
+                activeCard.classList.remove('card__flipped');
+                e.target.classList.remove('card__flipped');
+                activeCard = null;
+                setTimeout(() => {
+                  canClick = true;
+                }, 300);
+              }, 100);
+            }, 700);
+          }, 700);
+
+          movesNumber += 1;
+          printMoves(movesNumber);
+        }
+
+        // Mark a card as selected one
+      } else if (canClick) {
+        activeCard = e.target;
+        canClick = false;
+        activeCard.classList.add('card__flipped');
+        setTimeout(() => {
+          canClick = true;
+        }, 300);
       }
+    };
 
-      // Mark a card as selected one
-    } else {
-      activeCard = e.target;
-      activeCard.classList.add('card__flipped');
-    }
+    // Calling functions to start the game
+    shuffleCards();
+    printMoves(movesNumber);
+
+    // Remove all temporary classes and showing cards for 3 seconds
+    canClick = false;
+    cards.forEach((card) => {
+      card.classList.remove('shake', 'card__flipped');
+
+      // Timeout is needed to do properly opening animation to previously opened cards
+      setTimeout(() => {
+        card.classList.add('card__flipped');
+
+        // Timer to show cards at the start of the game
+        setTimeout(() => {
+          card.classList.remove('card__flipped');
+          setTimeout(() => {
+            canClick = true;
+          }, 300);
+        }, 1000);
+      }, 0);
+    });
+
+    // Clicking on cards
+    cards.forEach((card) => {
+      card.removeEventListener('click', clicked);
+      card.addEventListener('click', clicked);
+    });
   };
 
-  // Calling functions to start the game
-  shuffleCards();
-  printMoves(movesNumber);
-
-  // Remove all temporary classes and showing cards for 3 seconds
-  cards.forEach((card) => {
-    card.classList.remove('shake');
-    card.classList.add('card__flipped');
-    setTimeout(() => {
-      const currentCard = card;
-      currentCard.classList.remove('card__flipped');
-    }, 1000);
-  });
-
-  // Clicking on cards
-  cards.forEach(card => card.addEventListener('click', clicked));
-};
-
-// Clicking on New Game button
-buttonNewGame.addEventListener('click', newGame);
+  // Clicking on New Game button
+  buttonNewGame.addEventListener('click', newGame);
+}());
